b34c5c7ddcddbf35363d662ccdb27afe1c496c5a
Sven <bitbucket@frechi.de>
2025-12-27 19:26:39 +0100

    refactorings

--- IotPlatformDemo.API/Program.cs
+++ IotPlatformDemo.API/Program.cs
@@ -13,9 +13,6 @@ var env = builder.Environment;
 var configuration = builder.Configuration;
 var isDevelopment = env.IsDevelopment();
 
-if (isDevelopment)
-    apiTitle += " (Development)";
-
 builder.Services.AddSingleton(RegistryManager.CreateFromConnectionString(configuration.GetValue<string>("iothub:connectionString")));
 
 builder.Services.AddControllers();
@@ -24,19 +21,19 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(builder.Configuration);
 
+var baseUrl = $"{configuration["AzureAd:Instance"]}/{configuration["AzureAd:TenantId"]}";
+
 builder.Services.AddSwaggerGen(options =>
 {
     var scopes = new Dictionary<string, string>
     {
         { configuration["SwaggerAuth:Scope"], "ReadWrite" },
-        // add any custom scopes necessary, in place of or in addition to Default, e.g.
-        // { $"api://{builder.Configuration["AzureAd:ClientId"]}/access_as_user", "Access as User" },
         { "openid", "openid" },
     };
     var authFlow = new OpenApiOAuthFlow
     {
-        AuthorizationUrl = new Uri($"https://svfriotdemo.ciamlogin.com/0484e3c3-e93f-4d52-bbb0-4af88d1c1b44/oauth2/v2.0/authorize"),
-        TokenUrl = new Uri($"https://svfriotdemo.ciamlogin.com/0484e3c3-e93f-4d52-bbb0-4af88d1c1b44/oauth2/v2.0/token"),
+        AuthorizationUrl = new Uri($"{baseUrl}/oauth2/v2.0/authorize"),
+        TokenUrl = new Uri($"{baseUrl}/oauth2/v2.0/token"),
         Scopes = scopes,
     };
     var openApiSecurityScheme = new OpenApiSecurityScheme
@@ -69,8 +66,6 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// Serve the Swagger UI at the app root ("/") by setting RoutePrefix to empty.
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
@@ -83,12 +78,6 @@ app.UseSwaggerUI(options =>
 });
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-//if (isDevelopment)
-//    app.MapControllers().WithMetadata(new AllowAnonymousAttribute());
-//else
-    app.MapControllers();
-
+app.MapControllers();
 app.Run();
